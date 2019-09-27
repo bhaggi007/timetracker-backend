d16daed71eb2d2f03421d0c06ef1d6502abc8a25
bhaggi007 <51153542+bhaggi007@users.noreply.github.com>
2019-09-27 16:35:56 +0530

    changing cors origin settings

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(adminRouter);
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(function (req, res, next) {
     req.setTimeout(0);
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", true);
     next();
